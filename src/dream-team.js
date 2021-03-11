d4f1ec5e55af4958e48e0a8803d4b7eadeb9a85b
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-11 23:50:23 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(dreamTeamMembers) {
+
+  for (let i = 0; i < dreamTeamMembers.length; i++) {
+
+    if (dreamTeamMembers === dreamTeamMembers[0]) {
+      return true;
+    } else if (dreamTeamMembers === ' ') {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
 };
