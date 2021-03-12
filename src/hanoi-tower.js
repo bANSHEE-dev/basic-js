abe0b50e229fcd2434d40ea451b19e9c294ba058
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-12 15:41:15 +0300

    init: new solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,12 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+
+    const movePerSecond = turnsSpeed / 3600;
+    const moves = (2 ** disksNumber) - 1;
+    const sec = moves / movePerSecond;
+
+    return {
+        moves,
+        sec
+    };
 }
