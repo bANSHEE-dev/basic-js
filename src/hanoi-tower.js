9eeb979d440b82490a9dbb75315da08d4ce7adf4
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-12 16:12:51 +0300

    fix: change names

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,12 +1,10 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-
-
-    const turnsPerSecond = turnsSpeed / 3600;
-    const turns = (2 ** disksNumber) - 1;
-    const seconds = turns / turnsPerSecond;
+    const movePerSecond = turnsSpeed / 3600;
+    const moves = (2 ** disksNumber) - 1;
+    const sec = moves / movePerSecond;
 
     return {
-        turns,
-        seconds
+        moves,
+        sec
     };
 }
