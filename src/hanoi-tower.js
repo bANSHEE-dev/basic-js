80c043e8e678bff3e8fbac3aa89e21bc515bc334
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-12 15:44:14 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,12 +1,12 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
 
 
-    const movePerSecond = turnsSpeed / 3600;
-    const moves = (2 ** disksNumber) - 1;
-    const sec = moves / movePerSecond;
+    const turnsPerSecond = turnsSpeed / 3600;
+    const turns = (2 ** disksNumber) - 1;
+    const seconds = turns / turnsPerSecond;
 
     return {
-        moves,
-        sec
+        turns,
+        seconds
     };
 }
