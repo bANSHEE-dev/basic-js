3996c6613a4ef47d50230b0e2fda018ca4498230
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-13 07:55:50 +0300

    init: new solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,12 @@
+const hasNestedArr = (arr) => arr.some(item => Array.isArray(item));
+
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let depth = 1;
+        if(hasNestedArr(arr)) {
+            const newArr = arr.reduce((acc, cur) => acc.concat(cur), []);
+            return depth + this.calculateDepth(newArr);
+        }
+        return depth
     }
 };
